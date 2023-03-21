8b2c537f249dd74fe74d89935c229b4a0c5667f3
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2023-03-21 22:59:27 +0200

    nav, footer, todo-list components and basic router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
+const routes: Routes = [
+  {path:'', component: TodoListComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    NavBarComponent,
+    FooterComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
