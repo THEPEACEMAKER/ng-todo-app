875d47cd98a2171f67a224aa758528e9c64535e0
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2023-03-22 13:39:34 +0200

    get todos from the todosService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { Page404Component } from './page404/page404.component';
 import { TodoDetailsComponent } from './todo-details/todo-details.component';
 import { AuthService } from './auth/auth.service'
 import { AuthGuard } from './auth/auth.guard'
+import { TodosService } from './todos.service'
 
 const routes: Routes = [
   {path:'', component: TodoListComponent},
@@ -34,7 +35,7 @@ const routes: Routes = [
     FormsModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [AuthService, AuthGuard],
+  providers: [AuthService, AuthGuard, TodosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
